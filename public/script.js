4a14810c03b6d4da101ca75b0d648a42baf1b211
skashyap08 <sakshikashyap9876@gmail.com>
2026-06-27 12:02:24 +0530

    updated script.js

---
+++ public/script.js
@@ -0,0 +1,11 @@
+function go(route){
+    window.location.href=route;
+}
+
+function toggleTheme(){
+    document.body.classList.toggle("dark");
+}
+
+window.onload=()=>{
+    console.log("Website Loaded");
+}
