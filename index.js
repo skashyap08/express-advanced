1d19c02920c9107ff8794ec87c92fe01aef4c8fe
skashyap08 <sakshikashyap9876@gmail.com>
2026-06-27 11:55:42 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ const path = require("path");
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+// Static Folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "views", "index.html"));
+});
